66bbdfe8c71ceb839d3739a2fbe8908c7afc958b
Andreas Lohr <andr3as.lohr@gmail.com>
2014-05-28 21:25:36 +0200

    added days

--- week1.R
+++ week1.R
@@ -1,9 +1,21 @@
 
 readActivity <- function() {
-  
-  activity <- read.csv("activity.csv", colClasses = c("integer", "Date", "integer"))
-  
-  within(activity, {
-    date = as.factor(date)
-  })
+  read.csv("activity.csv", colClasses = c("integer", "Date", "integer"))
+}
+
+createDays <- function() {
+  read.table(header = T, stringsAsFactors = T, text = "
+    weekday   type
+    Monday    weekday
+    Tuesday   weekday
+    Wednesday weekday
+    Thursday  weekday
+    Friday    weekday
+    Saturday  weekend
+    Sunday    weekend
+  ")
+}
+
+mergeDays <- function() {
+  a$day <- days[as.factor(weekdays(a$date)),2]
 }
